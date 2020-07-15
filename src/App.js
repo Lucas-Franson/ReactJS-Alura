25ef239f47e2d3cba60d2bfca2413625dca0915d
Lucas-Franson <lucasfranson.8@hotmail.com>
2020-07-15 13:17:32 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
+import Tabela from './Tabela';
+import 'materialize-css/dist/css/materialize.min.css';
+import './App.css';
+import PopUp from './PopUp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Header';
+import Form from './Formulario';
+
+class App extends Component {
+  state = {
+    autores: [
+      {
+        nome: 'Paulo',
+        livro: 'React',
+        preco: '1000'
+      },
+      {
+        nome: 'Daniel',
+        livro: 'Java',
+        preco: '99'
+      },
+      {
+        nome: 'Marcos',
+        livro: 'Design',
+        preco: '150'
+      },
+      {
+        nome: 'Bruno',
+        livro: 'DevOps',
+        preco: '100'
+      }
+    ]
+  };
+
+  removeAutor = index => {
+    const { autores } = this.state;
+
+    this.setState(
+      {
+        autores: autores.filter((autor, posAtual) => {
+          return posAtual !== index;
+        })
+      }
+    );
+
+    PopUp.exibeMensagem("error", "Autor removido com sucesso");
+  }
+
+  escutadorDeSubmit = autor => {
+    this.setState({ autores: [...this.state.autores, autor] });
+    PopUp.exibeMensagem("success", "Autor adicionado com sucesso");
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header />
+        <div className="container mb-10">
+          <Tabela autores={this.state.autores} removeAutor={this.removeAutor} />
+          <Form escutadorDeSubmit={this.escutadorDeSubmit} />
+        </div>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/FormValidator.js
@@ -0,0 +1,41 @@
+import validador from 'validator';
+
+export default class FormValidator {
+
+    constructor(validacoes) {
+        this.validacoes = validacoes;
+    }
+
+    valida(state) {
+
+        let validacao = this.valido();
+
+        this.validacoes.forEach(regra => {
+            const campoValor = state[regra.campo.toString()];
+            const args = regra.args || [];
+            const metodoValidacao = typeof regra.metodo === 'string' ?
+                validador[regra.metodo] : regra.metodo;
+
+            if (metodoValidacao(campoValor, ...args, state) !== regra.validoQuando) {
+                validacao[regra.campo] = {
+                    isInvalid: true,
+                    message: regra.mensagem
+                }
+
+                validacao.isValid = false;
+            }
+        });
+        return validacao;
+    }
+
+    valido() {
+        const validacao = {};
+
+        this.validacoes.map(regra => {
+            validacao[regra.campo] = { isInvalid: false, message: '' }
+        });
+
+        return { isValid: true, ...validacao };
+    }
+
+}
